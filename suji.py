3eb300226d0b89821809244c238908a96c008948
Marshall Ward <marshall.ward@gmail.com>
2014-08-10 13:41:19 +1000

    Refactored the higher digits outside of minutes_to_kana

--- suji.py
+++ suji.py
@@ -4,31 +4,40 @@ import datetime
 def minute_to_kana(minutes):
 
     # Check input
-    if not type(minutes) is int or minutes < 0 or minutes >= 60:
-        raise ValueError('Input must be an integer between 0 and 59'
-                         ' (inclusive).')
+    if not type(minutes) is int or minutes < 0:
+        raise ValueError('Input must be a non-negative integer.')
 
-    with open('time.yaml') as t_cnt_file:
-        t_cnt = yaml.load(t_cnt_file)
+    with open('time.yaml') as time_file:
+        time_file = yaml.load(time_file)
 
-    # "decaminute" and its remainder
-    damin_q = minutes / 10
-    damin_r = minutes % 10
+    m_kana = decakana(minutes)
 
-    # Set the multiple of 10 (if present)
-    m_kana = ''
-    if minutes >= 20:
-        m_kana = t_cnt['digits'][damin_q]
-
-    if minutes >= 10 and damin_r > 0:
-        m_kana += t_cnt['digits'][10]
-
-    if minutes > 0:
-        if damin_r == 0:
-            m_kana += t_cnt['minutes'][10]
-        else:
-            m_kana += t_cnt['minutes'][damin_r]
+    if minutes > 0 and minutes % 10 == 0:
+        m_kana += t_cnt['minutes'][10]
     else:
-        m_kana = t_cnt['minutes'][0]
+        m_kana += t_cnt['minutes'][minutes % 10]
 
-    print(m_kana)
+    return m_kana
+
+
+def decakana(num):
+    """Return the kana for the value of ``num`` greater than 10."""
+
+    # TODO: Kanji readings
+    # TODO: num >= 100
+    assert num < 100
+
+    with open('digits.yaml') as digits_file:
+        digits = yaml.load(digits_file)
+
+    danum_q = num / 10
+    danum_r = num % 10
+
+    danum_kana = ''
+    if num >= 20:
+        danum_kana += digits[danum_q]
+
+    if num >= 10 and danum_r > 0:
+        danum_kana += digits[10]
+
+    return danum_kana
